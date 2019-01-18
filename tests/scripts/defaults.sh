f25b34a20ad114dfcab5f552622d65b403f53cc0
John Carlyle-Clarke <john.carlyleclarke@deliveroo.co.uk>
2019-01-18 12:06:11 +0000

    Add descriptions to tests, and a few more tests

--- tests/scripts/defaults.sh
+++ tests/scripts/defaults.sh
@@ -3,17 +3,28 @@ set -uo pipefail
 
 source common.sh
 
+title "Testing wih default settings"
+
 # All healthcheck settings at default
 start_healthcheck
 start_pgbouncer
 
+desc "PGBouncer is running, healthcheck should return OK"
 test_ok_healthcheck
+desc "Debug endpoints disabled by default, should return Error"
 test_notok_debug
+desc "No credentials set, so status endpoints should return Error"
 test_notok_status
 
 stop_pgbouncer
+desc "PGBouncer is stopped, healthcheck should return Error"
 test_notok_healthcheck
+desc "PGbouncer is stopped, status endpints should return Error"
 test_notok_status
+
+start_pgbouncer
+desc "PGBouncer is running again, healthcheck should return OK"
+test_ok_healthcheck
 
 end_tests
 summary
